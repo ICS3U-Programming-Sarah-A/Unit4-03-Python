dd5c30a04921067d89e72b9edb7a726c0a333d49
Ubuntu <ubuntu@ip-172-31-91-254.ec2.internal>
2022-04-24 02:06:22 +0000

    Made a program that calculates the power of loop_counter using a for loop.

--- power_of_two.py
+++ power_of_two.py
@@ -2,24 +2,23 @@
 
 # Created by: Sarah
 # Created on: Apr, 20, 2022
-# This program asks the user to enter a number. It then displays the 
-# power of two of each number from 0 until that number using for loops. 
-
+# This program asks the user to enter a number. It then displays the
+# power of two of each number from 0 until that number using for loops.
 
 
 def main():
     # initialize the loop counter and power_of_two
     counter = 0
     power_of_two = 0
-    
+
     # get the user number as a string
-    user_number_string = input("Enter a positive number: ")
+    user_number_string = input("Enter a whole number: ")
     print("")
 
     try:
         # converts user input to integer
         user_number_int = int(user_number_string)
-        # calculate the factorial of the user using loops.
+        # calculate the power of counter using a for loop.
         if user_number_int >= 0:
             for counter in range(user_number_int + 1):
                 power_of_two = counter**2
@@ -34,4 +33,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
